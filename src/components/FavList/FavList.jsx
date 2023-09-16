1915b67f9f8da8310f61ee68a6d5f86f8b68f2eb
Nataliia-Shevchenko <nata_yuna@ukr.net>
2023-09-16 20:48:46 +0300

    add FavList

---
+++ src/components/FavList/FavList.jsx
@@ -0,0 +1,18 @@
+import CarCard from "../CarCard/CarCard";
+
+const FavList =({favs}) =>{
+
+
+    return (
+        <div className="container mx-auto px-128 grid grid-cols-4 gap-x-7 gap-y-12 list-none mt-10">
+        {favs &&
+          favs.map((car) => (
+            <li className="box-border min-h-106 max-w-68 p-0 border-0" key={car.id}>
+              <CarCard car={car} />
+            </li>
+          ))}
+      </div>
+    )
+}
+
+export default FavList;
